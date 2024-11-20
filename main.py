3be5de87a9cf18c6a9ddee72cb35775f359263cc
Simon Edmunds <sjedmunds04@gmail.com>
2024-11-20 11:26:09 -0600

    Implemented Lotka-Volterra model for population

--- main.py
+++ main.py
@@ -19,61 +19,109 @@ class PopulationWindow(QMainWindow):
         plot_widget = pg.PlotWidget()
         plot_area.addWidget(plot_widget)
         self.plot_item: pg.PlotItem = plot_widget.getPlotItem()
+        self.plot_item.addLegend()
+
+        # Set axis labels
+        self.plot_item.setLabel('left', 'Population', units='individuals')  # Label for y-axis
+        self.plot_item.setLabel('bottom', 'Time', units='time steps')  # Label for x-axis
 
         # Set up validators for the line edits to allow only float values
         float_validator = QDoubleValidator(self)
-        self.ui.initial_population_line_edit.setValidator(float_validator)
-        self.ui.max_population_line_edit.setValidator(float_validator)
+        self.ui.initial_prey_population_line_edit.setValidator(float_validator)
+        self.ui.initial_predator_population_line_edit.setValidator(float_validator)
         self.ui.growth_rate_line_edit.setValidator(float_validator)
-        self.ui.steps_line_edit.setValidator(float_validator)
+        self.ui.starve_rate_line_edit.setValidator(float_validator)
+        self.ui.predator_reproduction_rate_line_edit.setValidator(float_validator)
+        self.ui.predation_rate_line_edit.setValidator(float_validator)
+        self.ui.end_time_line_edit.setValidator(float_validator)
+        self.ui.time_step_line_edit.setValidator(float_validator)
 
         # Connecting signals
-        self.ui.initial_population_line_edit.editingFinished.connect(self.factors_changed)
-        self.ui.max_population_line_edit.editingFinished.connect(self.factors_changed)
+        self.ui.initial_prey_population_line_edit.editingFinished.connect(self.factors_changed)
+        self.ui.initial_predator_population_line_edit.editingFinished.connect(self.factors_changed)
         self.ui.growth_rate_line_edit.editingFinished.connect(self.factors_changed)
-        self.ui.steps_line_edit.editingFinished.connect(self.factors_changed)
+        self.ui.starve_rate_line_edit.editingFinished.connect(self.factors_changed)
+        self.ui.predation_rate_line_edit.editingFinished.connect(self.factors_changed)
+        self.ui.predator_reproduction_rate_line_edit.editingFinished.connect(self.factors_changed)
+        self.ui.end_time_line_edit.editingFinished.connect(self.factors_changed)
+        self.ui.time_step_line_edit.editingFinished.connect(self.factors_changed)
+
         self.ui.reset_button.pressed.connect(self.reset)
 
         # Variables for plotting
-        self.x: np.ndarray | None = None
-        self.y: np.ndarray | None = None
+        self.prey_data: list[np.ndarray | None] = [None, None]
+        self.predator_data: list[np.ndarray | None] = [None, None]
 
-        # Initialize default values
-        self.default_initial_population = self.initial_population = 10
-        self.default_max_population = self.max_population = 250
-        self.default_growth_rate = self.growth_rate = 0.1
-        self.default_num_steps = self.num_steps = 100
+        # Initialize default values for predator-prey model
+        self.default_initial_prey_population = self.initial_prey_population = 40
+        self.default_initial_predator_population = self.initial_predator_population = 9
+        self.default_prey_growth_rate = self.prey_growth_rate = 0.1
+        self.default_predator_starve_rate = self.predator_starve_rate = 0.1
+        self.default_predator_reproduction_rate = self.predator_reproduction_rate = 0.01
+        self.default_predation_rate = self.predation_rate = 0.02
+        self.default_end_time = self.end_time = 50
+        self.default_time_step = self.time_step = 0.0001
 
         # Populate the plot initially
         self.factors_changed(True)
 
     def factors_changed(self, override_text: bool = False):
-        self.plot_item.clear()
         if override_text:
-            self.ui.initial_population_line_edit.setText(str(self.initial_population))
-            self.ui.max_population_line_edit.setText(str(self.max_population))
-            self.ui.growth_rate_line_edit.setText(str(self.growth_rate))
-            self.ui.steps_line_edit.setText(str(self.num_steps))
+            self.ui.initial_prey_population_line_edit.setText(str(self.initial_prey_population))
+            self.ui.initial_predator_population_line_edit.setText(str(self.initial_predator_population))
+            self.ui.growth_rate_line_edit.setText(str(self.prey_growth_rate))
+            self.ui.starve_rate_line_edit.setText(str(self.predator_starve_rate))
+            self.ui.predator_reproduction_rate_line_edit.setText(str(self.predator_reproduction_rate))
+            self.ui.predation_rate_line_edit.setText(str(self.predation_rate))
+            self.ui.end_time_line_edit.setText(str(self.end_time))
+            self.ui.time_step_line_edit.setText(str(self.time_step))
         else:
-            self.initial_population = int(self.ui.initial_population_line_edit.text())
-            self.max_population = int(self.ui.max_population_line_edit.text())
-            self.growth_rate = float(self.ui.growth_rate_line_edit.text())
-            self.num_steps = int(self.ui.steps_line_edit.text())
+            self.initial_prey_population = int(self.ui.initial_prey_population_line_edit.text())
+            self.initial_predator_population = int(self.ui.initial_predator_population_line_edit.text())
+            self.prey_growth_rate = float(self.ui.growth_rate_line_edit.text())
+            self.predator_starve_rate = float(self.ui.starve_rate_line_edit.text())
+            self.predator_reproduction_rate = float(self.ui.predator_reproduction_rate_line_edit.text())
+            self.predation_rate = float(self.ui.predation_rate_line_edit.text())
+            self.end_time = int(float(self.ui.end_time_line_edit.text()))
+            self.time_step = float(self.ui.time_step_line_edit.text())
 
-        self.x = np.linspace(0, self.num_steps, self.num_steps * 10)
+        time = np.arange(0, self.end_time, self.time_step)
+        self.prey_data[0] = time
+        self.predator_data[0] = time
 
-        # Using logistic growth model
-        self.y = self.max_population / (
-                1 + ((self.max_population - self.initial_population) / self.initial_population) * np.exp(
-            -self.growth_rate * self.x))
+        # Variables to store population values over time
+        prey_population_values = [self.initial_prey_population]
+        predator_population_values = [self.initial_predator_population]
 
-        self.plot_item.plot(self.x, self.y, pen=pg.mkPen('b'))
+        for i in range(1, len(time)):
+            dN = prey_population_values[i - 1] * (
+                        self.prey_growth_rate - self.predation_rate * predator_population_values[i - 1])
+            dP = -predator_population_values[i - 1] * (
+                        self.predator_starve_rate - self.predator_reproduction_rate * prey_population_values[i - 1])
+
+            next_N = prey_population_values[i-1] + dN * self.time_step
+            next_P = predator_population_values[i-1] + dP * self.time_step
+
+            # Store populations for plotting
+            prey_population_values.append(next_N)
+            predator_population_values.append(next_P)
+
+        self.prey_data[1] = prey_population_values
+        self.predator_data[1] = predator_population_values
+
+        # Plotting data
+        self.plot_item.clear()
+        self.plot_item.plot(*self.prey_data, pen=pg.mkPen('b'), name="Prey Population")
+        self.plot_item.plot(*self.predator_data, pen=pg.mkPen('r'), name="Predator Population")
 
     def reset(self):
-        self.initial_population = self.default_initial_population
-        self.max_population = self.default_max_population
-        self.growth_rate = self.default_growth_rate
-        self.num_steps = self.default_num_steps
+        self.initial_prey_population = self.default_initial_prey_population
+        self.initial_predator_population = self.default_initial_predator_population
+        self.prey_growth_rate = self.default_prey_growth_rate
+        self.predator_starve_rate = self.default_predator_starve_rate
+        self.predator_reproduction_rate = self.default_predator_reproduction_rate
+        self.predation_rate = self.default_predation_rate
+        self.end_time = self.default_end_time
         self.factors_changed(True)
 
 
